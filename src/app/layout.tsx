dbad075df2e3a0652c9090d71fd603da3b629429
Graeme Tudhope <nanogy98@gmail.com>
2026-08-01 23:33:47 +0100

    Import Script from next/script in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { Archivo, Newsreader, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 import { site } from "@/lib/site";
